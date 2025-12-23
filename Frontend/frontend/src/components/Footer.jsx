4ea28d7aa74e697a6c0fcc083f411d525e799385
Rishitha <rishithalattupally01@gmail.com>
2025-12-23 11:19:33 +0530

    Delete Frontend/frontend directory

--- Frontend/frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer style={{ textAlign: 'center', padding: '1rem', backgroundColor: 'white', color: 'black' }}>
-      <p>&copy; 2025 Freelance Finder. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
